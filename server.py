d9b948aeb61d09cb3c3043e5b18497c0e2389762
Sam Gibson <sam@ifdown.net>
2014-11-29 10:11:51 +1100

    Limit rotation speed

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ import serial
 
 import sentry
 
+MAX_SPEED = 6
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 s.bind(("", 7997))
@@ -14,11 +16,28 @@ print "listening on 7997"
 def throttle():
     pass
 
+current_pan = 90
+current_tilt = 90
+
 def pan(gun, v):
-    gun.pan_to(v)
+    global current_pan
+    vel = abs(current_pan - v)
+    if v < current_pan:
+        current_pan -= min(MAX_SPEED, vel)
+    else:
+        current_pan += min(MAX_SPEED, vel)
+
+    gun.pan_to(current_pan)
 
 def tilt(gun, v):
-    gun.tilt_to(v)
+    global current_tilt
+    vel = abs(current_tilt - v)
+    if v < current_tilt:
+        current_tilt -= min(MAX_SPEED, vel)
+    else:
+        current_tilt += min(MAX_SPEED, vel)
+
+    gun.tilt_to(current_tilt)
 
 def server(sock):
     gun = sentry.Sentry()
